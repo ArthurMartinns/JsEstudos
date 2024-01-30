26abbf9b25291ca3afc7b7023ad15f95537774cc
Arthur Martins <143364331+ArthurMartinns@users.noreply.github.com>
2024-01-30 16:15:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+let firstnumber = prompt("Digite o primeiro numero:")
+let secondnumber = prompt("Digite o segundo numero:")
+
+
+firstnumber = Number(firstnumber)
+secondnumber = Number(secondnumber)
+
+
+const sum = firstnumber + secondnumber
+const sub = firstnumber - secondnumber
+const mult = firstnumber * secondnumber
+const div = firstnumber / secondnumber
+const rest = firstnumber % secondnumber
+
+
+alert("A soma é" + sum)
+alert("A subtração é" + sub)
+alert("A multiplicação é" + mult)
+alert("A divisão é" + div)
+alert("O resto é:" + rest)
